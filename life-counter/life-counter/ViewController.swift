85ad6e19ab339aad2ce20cf5cc70a640cbadb762
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-09-10 15:51:00 +0900

    first commit

---
+++ life-counter/life-counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  life-counter
+//
+//  Created by 長坂豪士 on 2019/09/10.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
